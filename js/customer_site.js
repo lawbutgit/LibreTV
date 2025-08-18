2a4014093b5205e1d3698e9f8c580560503581b1
lawbutgit <90968571+lawbutgit@users.noreply.github.com>
2025-08-18 10:36:38 +0800

    Update customer_site.js

--- js/customer_site.js
+++ js/customer_site.js
@@ -3,6 +3,40 @@ const CUSTOMER_SITES = {
         api: 'https://www.qiqidys.com/api.php/provide/vod',
         name: '七七资源',
     }
+    
+   dyttzy: {
+        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        name: '电影天堂资源',
+    }
+
+
+    ruyi: {
+        api: 'https://cj.rycjapi.com/api.php/provide/vod',
+        name: '如意资源',
+    }
+
+
+    bfzy: {
+        api: 'https://bfzyapi.com/api.php/provide/vod'
+        name: '暴风资源',
+    }
+
+    tyyszy: {
+        api: 'https://tyyszy.com/api.php/provide/vod',
+        name: '天涯资源',
+   }
+
+
+    xiaomaomi: {
+        api: 'https://zy.xmm.hk/api.php/provide/vod',
+        name: '小猫咪资源',
+    }
+
+
+    ffzy: {
+        api: 'http://ffzy5.tv/api.php/provide/vod',
+        name: '非凡影视',
+    }
 };
 
 // 调用全局方法合并
